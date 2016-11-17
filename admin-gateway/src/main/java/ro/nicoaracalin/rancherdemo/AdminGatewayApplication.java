3e8605cfddb59797f42eb2caa3f2daaaee1fa3b3
calin_nicoara <calin.nicoara@esolutions.ro>
2016-11-17 12:00:28 +0200

    Final touches. Added discovery service setting to look for ip

--- admin-gateway/src/main/java/ro/nicoaracalin/rancherdemo/AdminGatewayApplication.java
+++ admin-gateway/src/main/java/ro/nicoaracalin/rancherdemo/AdminGatewayApplication.java
@@ -2,10 +2,12 @@ package ro.nicoaracalin.rancherdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
 public class AdminGatewayApplication {
 
 	public static void main(String[] args) {
